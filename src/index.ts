7eb67f8b906ff3558307eb3afac283a7dc9d8450
Torten Kiritsugu <me@torten.xyz>
2025-04-21 10:49:08 +0800

    change port so render recognizes it

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "@fastify/cors"
 import pino from "pino"
 import gameRoutes from "./routes/game";
 import 'dotenv/config'
+const port = process.env.PORT || 4000
 
 const server = Fastify({
     loggerInstance: pino({ level: 'info' })
@@ -18,7 +19,7 @@ const start = async () => {
     server.log.info('Loading environment variables', process.env)
     try {
         await server.listen({
-            port: process.env.PORT ? parseInt(process.env.PORT, 10) : 8080,
+            port,
         })
     } catch (err) {
         server.log.error(err);
